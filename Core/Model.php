37681f064be1703e7bd850d4bd873baaa412f216
pyiesone <pyiesone@users.noreply.github.com>
2019-05-11 19:15:19 +0800

    Initial Commit

---
+++ Core/Model.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Core;
+
+use PDO;
+use App\Config;
+
+/**
+ * Base model
+ *
+ */
+abstract class Model
+{
+
+    /**
+     * Get the PDO database connection
+     *
+     * @return mixed
+     */
+    protected static function getDB()
+    {
+        static $db = null;
+
+        if ($db === null) {
+            $dsn = 'mysql:host=' . Config::DB_HOST . ';dbname=' .
+                   Config::DB_NAME . ';charset=utf8';
+            $db = new PDO($dsn, Config::USERNAME, Config::PASSWORD);
+
+            // Throw an Exception when an error occurs
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+
+        return $db;
+    }
+}
